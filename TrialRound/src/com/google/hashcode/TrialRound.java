bc2928905c5fc106b8e755abd65d8dde13557656
wikimix <marouen.oueslati@gmail.com>
2014-04-05 00:59:54 +0200

    first commit

---
+++ TrialRound/src/com/google/hashcode/TrialRound.java
@@ -0,0 +1,12 @@
+package com.google.hashcode;
+
+
+public class TrialRound {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Trial Round - Google Hash Code 2014");
+
+	}
+
+}
